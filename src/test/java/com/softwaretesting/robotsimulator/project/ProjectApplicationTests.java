54606382edd2bfdef404eff5e8aee3d56de00799
sai-teja-concordia <114252385+sai-teja-concordia@users.noreply.github.com>
2023-07-09 12:05:00 -0400

    second commit

---
+++ src/test/java/com/softwaretesting/robotsimulator/project/ProjectApplicationTests.java
@@ -0,0 +1,8 @@
+package com.softwaretesting.robotsimulator.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ProjectApplicationTests {
+
+}
