efd5c6a5895e86598256225f45d3fb33bc5c911d
sai-teja-concordia <114252385+sai-teja-concordia@users.noreply.github.com>
2023-07-09 12:10:41 -0400

    Removed imports

--- src/main/java/com/softwaretesting/robotsimulator/project/ProjectApplication.java
+++ src/main/java/com/softwaretesting/robotsimulator/project/ProjectApplication.java
@@ -1,8 +1,5 @@
 package com.softwaretesting.robotsimulator.project;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 public class ProjectApplication {
 
 	public static void main(String[] args) {
