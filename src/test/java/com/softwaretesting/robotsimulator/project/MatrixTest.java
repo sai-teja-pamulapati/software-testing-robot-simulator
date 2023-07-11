037bbddeb9caad165fdf3e1a3765b71978c92835
Sai Teja Pamulapati <saiteja.pamulapati@mail.concordia.ca>
2023-07-11 08:25:14 -0400

    Added Test Cases for Move With Pen Up

--- src/test/java/com/softwaretesting/robotsimulator/project/MatrixTest.java
+++ src/test/java/com/softwaretesting/robotsimulator/project/MatrixTest.java
@@ -52,6 +52,135 @@ class MatrixTest {
     }
 
     @Test
+    public void moveWestPenUp() {
+        int[][] initialMatrix = this.matrix.getMatrix();
+        matrix.setXPosition(7);
+        matrix.setYPosition(4);
+        matrix.setDirection(DIRECTION.WEST);
+        matrix.setPenPosition(PEN_POSITION.UP);
+
+        this.matrix.move(5);
+
+        Assertions.assertEquals(2 , this.matrix.getXPosition());
+        Assertions.assertEquals(4 , this.matrix.getYPosition());
+        Assertions.assertEquals(DIRECTION.WEST , this.matrix.getDirection());
+        Assertions.assertEquals(PEN_POSITION.UP , this.matrix.getPenPosition());
+        Assertions.assertEquals(initialMatrix , this.matrix.getMatrix());
+    }
+
+
+    @Test
+    public void moveNorthPenUp() {
+        int[][] initialMatrix = this.matrix.getMatrix();
+        matrix.setXPosition(2);
+        matrix.setYPosition(4);
+        matrix.setDirection(DIRECTION.NORTH);
+        matrix.setPenPosition(PEN_POSITION.UP);
+
+        this.matrix.move(4);
+
+        Assertions.assertEquals(2 , this.matrix.getXPosition());
+        Assertions.assertEquals(8 , this.matrix.getYPosition());
+        Assertions.assertEquals(DIRECTION.NORTH , this.matrix.getDirection());
+        Assertions.assertEquals(PEN_POSITION.UP , this.matrix.getPenPosition());
+        Assertions.assertEquals(initialMatrix , this.matrix.getMatrix());
+    }
+
+
+    @Test
+    public void moveSouthPenUp() {
+        int[][] initialMatrix = this.matrix.getMatrix();
+        matrix.setXPosition(1);
+        matrix.setYPosition(8);
+        matrix.setDirection(DIRECTION.SOUTH);
+        matrix.setPenPosition(PEN_POSITION.UP);
+
+        this.matrix.move(7);
+
+        Assertions.assertEquals(1 , this.matrix.getXPosition());
+        Assertions.assertEquals(1 , this.matrix.getYPosition());
+        Assertions.assertEquals(DIRECTION.SOUTH , this.matrix.getDirection());
+        Assertions.assertEquals(PEN_POSITION.UP , this.matrix.getPenPosition());
+        Assertions.assertEquals(initialMatrix , this.matrix.getMatrix());
+    }
+
+    @Test
+    public void moveEastPenUpThrowException() {
+        int[][] initialMatrix = this.matrix.getMatrix();
+        matrix.setXPosition(1);
+        matrix.setYPosition(2);
+        matrix.setDirection(DIRECTION.EAST);
+        matrix.setPenPosition(PEN_POSITION.UP);
+
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class , () -> this.matrix.move(10));
+
+        Assertions.assertEquals("Robot exceeding the matrix boundaries" , exception.getMessage());
+        Assertions.assertEquals(1 , this.matrix.getXPosition());
+        Assertions.assertEquals(2 , this.matrix.getYPosition());
+        Assertions.assertEquals(DIRECTION.EAST , this.matrix.getDirection());
+        Assertions.assertEquals(PEN_POSITION.UP , this.matrix.getPenPosition());
+        Assertions.assertEquals(initialMatrix , this.matrix.getMatrix());
+    }
+
+    @Test
+    public void moveWestPenUpThrowException() {
+        int[][] initialMatrix = this.matrix.getMatrix();
+        matrix.setXPosition(7);
+        matrix.setYPosition(4);
+        matrix.setDirection(DIRECTION.WEST);
+        matrix.setPenPosition(PEN_POSITION.UP);
+
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class , () -> this.matrix.move(20));
+
+        Assertions.assertEquals("Robot exceeding the matrix boundaries" , exception.getMessage());
+        Assertions.assertEquals(7 , this.matrix.getXPosition());
+        Assertions.assertEquals(4 , this.matrix.getYPosition());
+        Assertions.assertEquals(DIRECTION.WEST , this.matrix.getDirection());
+        Assertions.assertEquals(PEN_POSITION.UP , this.matrix.getPenPosition());
+        Assertions.assertEquals(initialMatrix , this.matrix.getMatrix());
+    }
+
+
+    @Test
+    public void moveNorthPenUpThrowException() {
+        int[][] initialMatrix = this.matrix.getMatrix();
+        matrix.setXPosition(2);
+        matrix.setYPosition(4);
+        matrix.setDirection(DIRECTION.NORTH);
+        matrix.setPenPosition(PEN_POSITION.UP);
+
+
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class , () -> this.matrix.move(15));
+
+        Assertions.assertEquals("Robot exceeding the matrix boundaries" , exception.getMessage());
+        Assertions.assertEquals(2 , this.matrix.getXPosition());
+        Assertions.assertEquals(4 , this.matrix.getYPosition());
+        Assertions.assertEquals(DIRECTION.NORTH , this.matrix.getDirection());
+        Assertions.assertEquals(PEN_POSITION.UP , this.matrix.getPenPosition());
+        Assertions.assertEquals(initialMatrix , this.matrix.getMatrix());
+    }
+
+
+    @Test
+    public void moveSouthPenUpThrowException() {
+        int[][] initialMatrix = this.matrix.getMatrix();
+        matrix.setXPosition(1);
+        matrix.setYPosition(1);
+        matrix.setDirection(DIRECTION.SOUTH);
+        matrix.setPenPosition(PEN_POSITION.UP);
+
+
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class , () -> this.matrix.move(2));
+
+        Assertions.assertEquals("Robot exceeding the matrix boundaries" , exception.getMessage());
+        Assertions.assertEquals(1 , this.matrix.getXPosition());
+        Assertions.assertEquals(1 , this.matrix.getYPosition());
+        Assertions.assertEquals(DIRECTION.SOUTH , this.matrix.getDirection());
+        Assertions.assertEquals(PEN_POSITION.UP , this.matrix.getPenPosition());
+        Assertions.assertEquals(initialMatrix , this.matrix.getMatrix());
+    }
+
+    @Test
     public void printPosition() {
         matrix.setYPosition(3);
         matrix.setXPosition(6);
