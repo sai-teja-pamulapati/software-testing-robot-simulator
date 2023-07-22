2e0af094168d9638dc61f20174f9eeb36e24b1c8
Kavan050600 <k_jethav@live.concordia.ca>
2023-07-22 14:11:13 -0400

    Added display names for test cases

--- src/test/java/com/softwaretesting/robotsimulator/project/ProjectApplicationTest.java
+++ src/test/java/com/softwaretesting/robotsimulator/project/ProjectApplicationTest.java
@@ -3,6 +3,7 @@ package com.softwaretesting.robotsimulator.project;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -24,7 +25,7 @@ class ProjectApplicationTest {
     public void tearDown() {
         System.setOut(standardOut);
     }
-
+    @DisplayName("34. R1 -> Robot should be following the given instruction. (Input is capital M)")
     @Test
     void processCommandsInputCapitalM() {
         ProjectApplication.getMatrix().initializeMatrix(10);
@@ -35,7 +36,7 @@ class ProjectApplicationTest {
         Assertions.assertEquals(PEN_POSITION.UP , ProjectApplication.getMatrix().getPenPosition());
     }
 
-    @Test
+    @DisplayName("35. R1 -> Robot should be following the given instruction. (Input is small M)")
     void processCommandsInputSmallM() {
         ProjectApplication.getMatrix().initializeMatrix(10);
         ProjectApplication.processCommands("m 9");
@@ -44,7 +45,8 @@ class ProjectApplicationTest {
         Assertions.assertEquals(DIRECTION.NORTH , ProjectApplication.getMatrix().getDirection());
         Assertions.assertEquals(PEN_POSITION.UP , ProjectApplication.getMatrix().getPenPosition());
     }
-
+    
+    @DisplayName("36. R1 -> Robot should be following the given instruction. (Input is capital P)")
     @Test
     void processCommandsInputCapitalP() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -52,7 +54,7 @@ class ProjectApplicationTest {
         assertNotNull(outputStreamCaptor.toString().trim());
 
     }
-
+    @DisplayName("37. R1 -> Robot should be following the given instruction. (Input is small P)")
     @Test
     void processCommandsInputSmallP() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -60,6 +62,7 @@ class ProjectApplicationTest {
         assertNotNull(outputStreamCaptor.toString().trim());
     }
 
+    @DisplayName("38. R1 -> Robot should be following the given instruction. (Input is capital L)")
     @Test
     void processCommandsInputCapitalL() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -67,13 +70,15 @@ class ProjectApplicationTest {
         Assertions.assertEquals(DIRECTION.WEST , ProjectApplication.getMatrix().getDirection());
     }
 
+    @DisplayName("39. R1 -> Robot should be following the given instruction. (Input is small L)")
     @Test
     void processCommandsInputSmallL() {
         ProjectApplication.getMatrix().initializeMatrix(5);
         ProjectApplication.processCommands("l");
         Assertions.assertEquals(DIRECTION.WEST , ProjectApplication.getMatrix().getDirection());
     }
-
+    
+    @DisplayName("40. R1 -> Robot should be following the given instruction. (Input is capital R)")
     @Test
     void processCommandsInputCapitalR() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -81,7 +86,8 @@ class ProjectApplicationTest {
         Assertions.assertEquals(DIRECTION.EAST , ProjectApplication.getMatrix().getDirection());
 
     }
-
+    
+    @DisplayName("41. R1 -> Robot should be following the given instruction. (Input is small R)")
     @Test
     void processCommandsInputSmallR() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -89,34 +95,39 @@ class ProjectApplicationTest {
         Assertions.assertEquals(DIRECTION.EAST , ProjectApplication.getMatrix().getDirection());
     }
 
+    @DisplayName("42. R1 -> Robot should be following the given instruction. (Input is capital U)")
     @Test
     void processCommandsInputCapitalU() {
         ProjectApplication.getMatrix().initializeMatrix(5);
         ProjectApplication.processCommands("U");
         Assertions.assertEquals(PEN_POSITION.UP , ProjectApplication.getMatrix().getPenPosition());
     }
-
+    
+    @DisplayName("43. R1 -> Robot should be following the given instruction. (Input is small U)")
     @Test
     void processCommandsInputSmallU() {
         ProjectApplication.getMatrix().initializeMatrix(5);
         ProjectApplication.processCommands("u");
         Assertions.assertEquals(PEN_POSITION.UP , ProjectApplication.getMatrix().getPenPosition());
     }
-
+    
+    @DisplayName("44. R1 -> Robot should be following the given instruction. (Input is capital D)")
     @Test
     void processCommandsInputCapitalD() {
         ProjectApplication.getMatrix().initializeMatrix(5);
         ProjectApplication.processCommands("D");
         Assertions.assertEquals(PEN_POSITION.DOWN , ProjectApplication.getMatrix().getPenPosition());
     }
-
+    
+    @DisplayName("45. R1 -> Robot should be following the given instruction. (Input is small D)")
     @Test
     void processCommandsInputSmallD() {
         ProjectApplication.getMatrix().initializeMatrix(5);
         ProjectApplication.processCommands("d");
         Assertions.assertEquals(PEN_POSITION.DOWN , ProjectApplication.getMatrix().getPenPosition());
     }
-
+    
+    @DisplayName("46. R1 -> Robot should be following the given instruction. (Input is capital C)")
     @Test
     void processCommandsInputCapitalC() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -126,6 +137,7 @@ class ProjectApplicationTest {
                 .trim());
     }
 
+    @DisplayName("47. R1 -> Robot should be following the given instruction. (Input is small C)")
     @Test
     void processCommandsInputSmallC() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -135,6 +147,7 @@ class ProjectApplicationTest {
                 .trim());
     }
 
+    @DisplayName("48. R1 -> Robot should be following the given instruction. (Input is Invalid)")
     @Test
     void processCommandsInvalidInput2() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -142,13 +155,16 @@ class ProjectApplicationTest {
         Assertions.assertEquals("Invalid command!" , exception.getMessage());
     }
 
+    @DisplayName("49. R1 -> Robot should be following the given instruction. (Input is Invalid)")
     @Test
     void processCommandsInvalidInput() {
         ProjectApplication.getMatrix().initializeMatrix(5);
         IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class , () -> ProjectApplication.processCommands("  "));
         Assertions.assertEquals("Invalid command!" , exception.getMessage());
     }
-
+    
+    
+    @DisplayName("50. R1 -> Robot should be following the given instruction. (Input is Null )")
     @Test
     void processCommandsNullInput() {
         ProjectApplication.getMatrix().initializeMatrix(5);
@@ -156,12 +172,14 @@ class ProjectApplicationTest {
         Assertions.assertEquals("Invalid command!" , exception.getMessage());
     }
 
+    @DisplayName("51. R1 -> Robot should be following the given instruction. (First command is Null)")
     @Test
     void ProcessFirstCommandNull(){     		
     	IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class , () -> ProjectApplication.processFirstCommand(null));
     	Assertions.assertEquals("Invalid command!", exception.getMessage() );
     }
     
+    @DisplayName("52. R1 -> Robot should be following the given instruction. (Initialize the matrix)")
     @Test
     void ProcessFirstCommandInitializeMatrix(){
     	ProjectApplication.processFirstCommand("I 5");
@@ -171,7 +189,8 @@ class ProjectApplicationTest {
     	Assertions.assertEquals(DIRECTION.NORTH, ProjectApplication.getMatrix().getDirection());
     	Assertions.assertEquals(PEN_POSITION.UP, ProjectApplication.getMatrix().getPenPosition());
     }
-    
+   
+    @DisplayName("53. R1 -> Robot should be following the given instruction. (Input is Random)")
     @Test
     void ProcessFirstCommandRandomInput(){
     	
